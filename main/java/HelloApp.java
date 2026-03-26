b9015be58630e7545844d53fafae109bc01a03fe
pp2193-rgb <pp2193@srmist.edu.in>
2026-03-26 09:15:47 +0530

    Update HelloApp.java

--- main/java/HelloApp.java
+++ main/java/HelloApp.java
@@ -1,6 +1,16 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "Prithish";
-        System.out.println("Hello " + name + " !");
+        StringBuilder names = new StringBuilder();
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+        }
+        System.out.println("Hello, " + names + "!");
     }
 }
